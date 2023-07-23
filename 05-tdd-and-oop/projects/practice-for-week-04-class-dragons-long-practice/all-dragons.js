47b2796928e829f81b10ef44e1e1595c5e4cd01e
Yash <26933639+YashUppal@users.noreply.github.com>
2023-07-23 20:16:03 +0530

    add exercise solution

---
+++ 05-tdd-and-oop/projects/practice-for-week-04-class-dragons-long-practice/all-dragons.js
@@ -0,0 +1,38 @@
+const Dragon = require('./classes/dragon.js');
+const FriendlyDragon = require('./classes/friendly-dragon.js');
+const EvilDragon = require('./classes/evil-dragon.js');
+
+let falkor = new FriendlyDragon(
+  "Falkor",
+  "white",
+  [
+    "save Atreyu from the swamp",
+    "save Atreyu from the Nothing",
+    "scare the local bullies into a dumpster"
+  ],
+  "Bastian"
+);
+
+let smaug = new EvilDragon(
+  "Smaug",
+  "black",
+  [
+    "take over your mountain kingdom",
+    "steal all your dwarven gold",
+    "burn down your floating village"
+  ],
+  "Dwarf King"
+);
+
+const allDragons = Dragon.getDragons(falkor, smaug);
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports.falkor = falkor
+  module.exports.smaug = smaug
+  module.exports.allDragons = allDragons;
+} catch {
+  module.exports = null;
+}
