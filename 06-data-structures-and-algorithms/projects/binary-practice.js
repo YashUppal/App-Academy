123eb5dcb9abdece591ded745b2bfd2011f104d6
Yash <26933639+YashUppal@users.noreply.github.com>
2023-08-13 14:49:46 +0530

    add binary exercise solution

---
+++ 06-data-structures-and-algorithms/projects/binary-practice.js
@@ -0,0 +1,45 @@
+// In this exercise, translate the following values into the appropriate number base
+
+// Binary to base 10
+0b1010 // 10
+0b0011 // 13
+
+// Binary to hexadecimal
+0b1010 // A
+0b0011 // 3
+
+// Hexadecimal to base 10
+0xa1 // 161
+0xff // 255
+
+// Hexadecimal to binary
+0xa1 // 1010 0001
+0xff // 1111 1111
+
+// Base 10 to binary
+8 // 1000
+24 // 11000
+255 // 11111111
+
+// Base 10 to hexadecimal
+8 // 8
+24 // 18
+255 //
+
+// Base 10 to ASCII
+65 // A
+66 // B
+97 // a
+98 // b
+
+// Hexadecimal to ASCII
+0x41 // A
+0x42 // B
+0x61 // a
+0x62 // b
+
+// Binary to ASCII
+0b01000001 // A
+0b01000010 // B
+0b01100001 // a
+0b01100010 // b
